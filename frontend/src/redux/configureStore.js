62d65b27d3b695eee8b76a4529195be6ef7a3eb0
JaeWoongKang <kangwodnd@gmail.com>
2018-05-09 07:46:22 +0900

    development에 redux-logger설치하기

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,7 +2,23 @@ import { createStore , combineReducers, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import users from 'redux/modules/users';
 
+/** node js의 프로세스를 보여주는 변수
+ const env = env;*/
+
+/**현재 환경을 설명해주는 변수
+const env = process.env;**/
+/*현재 환경이 개발인지 운영인지 판별가능*/
+const env = process.env.NODE_ENV;
 const middlewares = [thunk];
+if(env === "development"){
+    //development일 때만 부르고 싶기 때문에 import 하지 않고 이렇게 작성함.
+    const {logger} = require("redux-logger");
+    middlewares.push(logger);
+}
+
+
+
+console.log(env);
 
 const reducer = combineReducers({
     users
