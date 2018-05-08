a3da82d13cf64a862e96e4f0c2bc4b883c829420
JaeWoongKang <kangwodnd@gmail.com>
2018-05-09 07:32:21 +0900

    리덕스 스토어에 미들웨어를 설치하는 법

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from "redux/configureStore";
+import 'index.css';
+import App from 'App';
 
+console.log(store.getState());
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-localStorage.setItem('bestCourse',"nomadAcamey");
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
---
+++ frontend/src/redux/modules/users.js
@@ -0,0 +1,26 @@
+//imports
+
+//actions
+
+//actions creators
+
+// inital state
+
+ const initialState = {
+     isLoggedIn : localStorage.getItem("jwt") || false
+}
+
+//reducer
+function reducer(state = initialState, action){
+    switch(action.type){
+        default:
+        return state;
+    }
+}
+// reducer functions
+
+//exports
+
+// reducer export
+
+export default reducer;
