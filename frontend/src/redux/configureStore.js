40cb9ff1d58a508e707b9eee9382c469be90095c
JaeWoongKang <kangwodnd@gmail.com>
2018-05-10 21:11:51 +0900

    redux-i18n 설치

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
 import { composeWithDevTools } from "redux-devtools-extension";
+import {i18nState } from "redux-i18n"
 import users from 'redux/modules/users';
 //import Reactotron from "ReactotronConfig"
 
@@ -34,7 +35,8 @@ console.log(env);
 
 const reducer = combineReducers({
     users,
-    routing: routerReducer
+    routing: routerReducer,
+    i18nState
 })
 
 let store;
